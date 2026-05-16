6cd39d1dd9661f5aad77a1ee3733c4de60466147
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2026-05-16 16:52:26 +0700

    feat comment, format code

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {Raffle} from "../src/Raffle.sol";
-// import {AddConsumer, CreateSubscription, FundSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function deployRaffle() public returns (Raffle, HelperConfig) {
@@ -12,6 +12,21 @@ contract DeployRaffle is Script {
         // local -> deploy mocks, get localconfig
         // sepolia -> get sepolia config from helperconfig
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+
+        // if (config.subscriptionId == 0) {
+        //     // สร้าง Subscription ID ใหม่
+        //     CreateSubscription createSubscription = new CreateSubscription();
+        //     (config.subscriptionId, config.vrfCoordinator) = createSubscription
+        //         .CreateSubscription(config.vrfCoordinator);
+
+        //     // เติมเงิน LINK เข้า Subscription เพื่อให้มีเงินจ่ายค่าสุ่ม
+        //     FundSubscription fundSubscription = new FundSubscription();
+        //     fundSubscription.fundSub(
+        //         config.vrfCoordinator,
+        //         config.subscriptionId,
+        //         config.link
+        //     ); // (เช็คตัวแปร link ใน config ด้วยนะ)
+        // }
 
         vm.startBroadcast();
 
@@ -24,6 +39,14 @@ contract DeployRaffle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+
+        // AddConsumer addConsumer = new AddConsumer();
+        // addConsumer.addConsumer(
+        //     address(raffle),
+        //     config.vrfCoordinator,
+        //     config.subscriptionId
+        // );
+
         return (raffle, helperConfig);
     }
 
