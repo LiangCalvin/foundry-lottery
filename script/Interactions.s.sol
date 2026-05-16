4e53d1178d599ec44e0a0d85e674cdca470d2b8a
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2026-05-16 17:24:54 +0700

    feat add AddConsumer contract

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {console} from "forge-std/console.sol";
 import {Raffle} from "../src/Raffle.sol";
-// import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {
     VRFCoordinatorV2_5Mock
 } from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
@@ -88,5 +88,44 @@ contract FundSubscription is Script, CodeConstants {
         uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
         address link = helperConfig.getConfig().link;
         fundSubscription(vrfCoordinator, subscriptionId, link);
+    }
+}
+
+contract AddConsumer is Script {
+    function addConsumerUsingConfig(
+        address mostRecentlyDeployedContract
+    ) public {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+
+        addConsumer(
+            mostRecentlyDeployedContract,
+            vrfCoordinator,
+            subscriptionId
+        );
+    }
+
+    function addConsumer(
+        address contractToAddtoVrf,
+        address vrfCoordinator,
+        uint256 subscriptionId
+    ) public {
+        console.log("Adding consumer to subscription ID:", subscriptionId);
+        console.log("Using VRF Coordinator:", vrfCoordinator);
+        console.log("On ChainId:", block.chainid);
+
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock(vrfCoordinator).addConsumer(
+            subscriptionId,
+            contractToAddtoVrf
+        );
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployedContract = DevOpsTools
+            .get_most_recent_deployment("Raffle", block.chainid);
+        addConsumerUsingConfig(mostRecentlyDeployedContract);
     }
 }
